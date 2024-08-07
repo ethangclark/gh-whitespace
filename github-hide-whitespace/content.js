58e85d487175d1093e1cdc3f24154dcf7456220a
Ethan Gregory Clark <ethan@onebrief.com>
2024-08-07 10:19:00 -0600

    get more specific when when to apply w=1

--- github-hide-whitespace/content.js
+++ github-hide-whitespace/content.js
@@ -1,6 +1,9 @@
 function hideWhitespace() {
   const url = new URL(window.location);
-  if (!url.searchParams.has("w")) {
+  if (
+    !url.searchParams.has("w") &&
+    url.href.split("/").length >= 7 // 7 because the format is https://github.com/owner/repo/mode/...identifierAndOtherStuff when we may want w=1, which means a min of 6 "/"s and thus a min of 7 chunks when split
+  ) {
     url.searchParams.set("w", "1");
     window.history.replaceState({}, "", url.toString());
     window.location.reload();
